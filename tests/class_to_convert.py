b0d2aea650c95f6ca44dfc5b5042695a231742dc
Anton Lobachev <anvlobachev@gmail.com>
2024-02-11 00:13:26 +0500

    Implement class for tests

---
+++ tests/class_to_convert.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+
+class ClassToConver:
+    def __init__(
+        self,
+        arg1,
+        arg2,
+        arb3,
+    ):
+
+        self.prop1 = None
+        self.prop2 = None
+        self.prop3 = None
+
+        self.prop1 = np.zeros(10, dtype=np.float64)
+        self.prop2 = np.zeros(10, dtype=np.float64)
+        self.prop3 = np.zeros(10, dtype=np.float64)
+
+        return
