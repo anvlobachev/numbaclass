cdb9bddc61acb19e1666a60576fe32fdcd8ff5f5
Anton Lobachev <anvlobachev@gmail.com>
2024-02-11 21:01:36 +0500

    Implement getsource via decorator

--- class_to_convert.py
+++ class_to_convert.py
@@ -1,14 +1,13 @@
 import numpy as np
 
 
-class ClassToConver:
-    def __init__(
-        self,
-        arg1,
-        arg2,
-        arb3,
-    ):
+from numbaclass import numbaclass
 
+
+@numbaclass
+class ClassToConvert:
+    def __init__(self, a):
+        self.b = a
         self.prop1 = None
         self.prop2 = None
         self.prop3 = None
@@ -17,4 +16,12 @@ class ClassToConver:
         self.prop2 = np.zeros(10, dtype=np.float64)
         self.prop3 = np.zeros(10, dtype=np.float64)
 
+        print("Class Init")
         return
+
+    def method1(self):
+        self.prop1[:] = 1
+        print(self.prop1)
+
+
+obj = ClassToConvert(1)
--- numbaclass.py
+++ numbaclass.py
@@ -1,1 +1,21 @@
+import inspect
 
+
+def numbaclass(func):
+    def wrapper(*args, **kwargs):
+        # Lets test retrieving sourcse code of function
+        _source = inspect.getsource(func)
+        print(_source)
+
+    return wrapper
+
+
+@numbaclass
+def test_func():
+    z = 0
+    for i in range(5):
+        z += 1
+    print(z)
+
+
+# test_func()
