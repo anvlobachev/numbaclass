f857575c1db998a22a05106eee880c66eed8f0d5
Anton Lobachev <anvlobachev@gmail.com>
2024-03-30 14:01:38 +0500

    Add decorated example for tests

---
+++ examples/example_decorated.py
@@ -0,0 +1,47 @@
+import numpy as np
+from numbaclass import numbaclass
+
+
+# @numbaclass
+@numbaclass(cache=False)
+class ExampleTwo:
+    def __init__(self, n):
+        """
+        Numbaclass will convert __init__ to wrapper function,
+        which will return jitted structref instance.
+        It's safe to use pure Python and any modules here to fetch data and
+        process values for structref inputs.
+
+        Note that, instance properties with  must be
+        compatible with Numba requirements.
+
+        """
+
+        self.prop1 = np.zeros(n, dtype=np.float64)
+        self.prop2 = np.zeros(n, dtype=np.float64)
+
+        self.prop1[:] = 7  # Property variation
+        print("Init done")
+
+    def incr_prop1(
+        self,
+        val: int,
+        # Multiline args
+        # Multiline args
+    ) -> None:  # Trailing comment and annotations
+        """
+        More comments
+        """
+        self.prop1[:] += val
+
+    # No type hints
+    def check_me(self):
+        print(self.prop1)
+
+
+obj = ExampleTwo(4)
+obj.incr_prop1(1)
+obj.incr_prop1(3)
+obj.check_me()
+
+print(obj)
