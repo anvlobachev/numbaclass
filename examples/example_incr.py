689354ddb04cbe21b30e396fbefb32b5848b29dc
Anton Lobachev <anvlobachev@gmail.com>
2024-07-23 16:19:43 +0500

    Add scalar property

--- examples/example_incr.py
+++ examples/example_incr.py
@@ -2,13 +2,15 @@ import numpy as np
 from numbaclass import numbaclass
 
 
-# @numbaclass(cache=True)
+@numbaclass(cache=True)
 class ExampleIncr:
-    def __init__(self, arr_):
+    def __init__(self, arr_, total_count):
         self.arr_ = arr_
+        self.total_count = total_count
 
     def incr(self, i, val):
         self.arr_[i] += val
+        self.total_count += val
 
     def get_count(self, i):
         return self.arr_[i]
@@ -20,8 +22,7 @@ def init(size):
     size: Provide size of one-dim array to store counts
     """
     arr_ = np.zeros(size, dtype=np.int64)
-    arr_[:] = 3
-    return ExampleIncr(arr_)
+    return ExampleIncr(arr_, 0)
 
 
 if __name__ == "__main__":
@@ -29,3 +30,9 @@ if __name__ == "__main__":
     obj.incr(0, 1)
     obj.incr(0, 1)
     print(obj.get_count(0))
+    print("obj.total_count: ", obj.total_count )
+    # reset total_count
+
+    obj.total_count = 0
+    print("obj.total_count: ", obj.total_count )
+
